09192d0380919a4edc9f6016905599190e515c7f
fedi <fedibenabdesslem46@gmail.com>
2025-07-18 17:52:41 +0100

    interaction avec angular

--- GestionProduit.API/Program.cs
+++ GestionProduit.API/Program.cs
@@ -7,33 +7,48 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// ?? Connexion PostgreSQL
+// Connexion PostgreSQL
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-// ?? Injection des dépendances
+// Injection des dépendances
 builder.Services.AddScoped<IProduitRepository, ProduitRepository>();
 builder.Services.AddScoped<IProduitService, ProduitService>();
 
-// ?? Ajouter les services API + Swagger
+// Ajouter les services API + Swagger
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// Ajouter la configuration CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
+
+
 var app = builder.Build();
 
-// ?? Swagger activé en mode développement
+// Swagger en mode développement
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
+// Utiliser CORS avant tout middleware qui traite les requêtes HTTP
+app.UseCors("AllowAll");
+
+
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
-// ?? Ajouter les contrôleurs
 app.MapControllers();
 
 app.Run();
